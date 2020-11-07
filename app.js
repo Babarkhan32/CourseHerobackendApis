2afb92c1e7c31c58635abc4e8a1ed6f80ff2f1fc
babar.0khan1@gmail.com <babar.0khan1@gmail.com>
2020-11-07 19:37:27 +0500

    my commit

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/api/users', usersRouter);
+app.use('/users', usersRouter);
 connectDB();
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
