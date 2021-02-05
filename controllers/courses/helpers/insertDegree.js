4a809c01613318fe6e6096ce868a9af9a033179e
babar.0khan1@gmail.com <babar.0khan1@gmail.com>
2021-02-05 16:53:39 +0500

    Course and degree work

--- controllers/courses/helpers/insertDegree.js
+++ controllers/courses/helpers/insertDegree.js
@@ -1,4 +1,4 @@
-const course = require('../../../models/courses');
+const courses = require('../../../models/courses');
 const moment = require('moment');
 
 exports.insertDegree = async (req, res) => {
@@ -9,8 +9,38 @@ exports.insertDegree = async (req, res) => {
     req.body.creation_date = moment().unix().toString();
     req.body.isDegree = true;
     req.body.facultyResume = pathsArr
-    console.log('reqBody', req.body)
-    await course.collection.insertOne(req.body)
+
+    let course=req.body.course.split(',');
+    req.body.course=course;
+
+    let online=req.body.online.split(',');
+    req.body.online=online;
+
+    let offline=req.body.offline.split(',');
+    req.body.offline=offline;
+
+    let facultyInfo=req.body.facultyInfo.split(',');
+    req.body.facultyInfo=facultyInfo;
+
+    let compulsory=req.body.compulsory.split(',');
+    req.body.compulsory=compulsory;
+
+
+    let optional=req.body.optional.split(",");
+    req.body.optional=optional;
+
+
+    let facultyInformation=[];
+    for(let i=0;i<req.body.facultyInfo.length;i++){
+      let obj={};
+        obj.facultyName=req.body.facultyInfo[i],
+        obj.facultyResume=req.body.facultyResume[i]
+
+        facultyInformation.push(obj);
+    }
+req.body.facultyInformation=facultyInformation;
+   
+    await courses.collection.insertOne(req.body)
     res.json({
       success: true,
       data: "Inserted"
