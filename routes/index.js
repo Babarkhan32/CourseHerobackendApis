08e6d347a46f16b71565e0f01b28485c5d5b6b8b
babar-at-657557213670 <babar@websailors.co>
2021-05-09 08:25:48 -0400

    Image Api for Admin access

--- routes/index.js
+++ routes/index.js
@@ -1,53 +1,82 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const multer = require('multer');
-
+const multer = require("multer");
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-
-        // console.log(file, "Req.files");
-        cb(null, 'uploads')
-    },
-    filename: function (req, file, cb) {
-        cb(null, `${new Date().getTime()}_${file.originalname}`)
-    }
+  destination: function (req, file, cb) {
+    // console.log(file, "Req.files");
+    cb(null, "uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${new Date().getTime()}_${file.originalname}`);
+  },
 });
-const uploads = multer({ storage })
-var course = require('../controllers/courses/courses')
-var teacher = require('../controllers/teacher/teacher')
-var institute = require('../controllers/institute/institute')
-var student = require('../controllers/students/students')
-var auth = require("../middleware/auth")
-var degree = require("../controllers/degree/degree")
+const uploads = multer({ storage });
+var course = require("../controllers/courses/courses");
+var teacher = require("../controllers/teacher/teacher");
+var institute = require("../controllers/institute/institute");
+var student = require("../controllers/students/students");
+var auth = require("../middleware/auth");
+var degree = require("../controllers/degree/degree");
 /* GET home page. */
-router.get('/', function (req, res) {
-    res.json({
-        data: "Home"
-    })
+router.get("/", function (req, res) {
+  res.json({
+    data: "Home",
+  });
 });
 
 /*Courses Api*/
 
-router.post('/courses/insert', auth, uploads.single('facultyResume'), course.insertCourses)
-router.get('/courses/:id', course.getCourses)
-router.get('/all/courses', course.getAllCourses)
-router.post('/search', course.searchCourse)
-router.post('/nav/search', course.navbarSearch)
+router.post(
+  "/courses/insert",
+  auth,
+  uploads.single("facultyResume"),
+  course.insertCourses
+);
+router.get("/courses/:id", course.getCourses);
+router.get("/all/courses", course.getAllCourses);
+router.post("/search", course.searchCourse);
+router.post("/nav/search", course.navbarSearch);
+router.post(
+  "/images",
+  uploads.array("facultyInformation"),
+  async function (req, res) {
+    console.log(req.files, "See it here");
 
+    res.json({
+      success: true,
+      data: req.files,
+    });
+  }
+);
 
 /*Teacher Apis*/
-router.post('/teacher/insert', uploads.single('imageSet'), teacher.insertTeacher);
-
+router.post(
+  "/teacher/insert",
+  uploads.single("imageSet"),
+  teacher.insertTeacher
+);
 
 /*Institute Apis*/
-router.post('/institute/insert', uploads.array('imageSet', 10), institute.insertInstitute);//changed to single by sulaiman
+router.post(
+  "/institute/insert",
+  uploads.array("imageSet", 10),
+  institute.insertInstitute
+); //changed to single by sulaiman
 
 /*Student Apis*/
-router.post('/student/insert', uploads.single('imageSet'), student.insertStudent);
-
+router.post(
+  "/student/insert",
+  uploads.single("imageSet"),
+  student.insertStudent
+);
 
 //degree Apis
-router.post('/degree/insert', auth, uploads.array('imageSet', 10), course.insertDegree)
+router.post(
+  "/degree/insert",
+  auth,
+  uploads.array("imageSet", 10),
+  course.insertDegree
+);
 
 module.exports = router;
