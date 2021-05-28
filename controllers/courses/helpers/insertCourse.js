eb9787ac15020d5d597d5b71a2b520b23f774ff0
babar-at-657557213670 <babar@websailors.co>
2021-05-28 12:05:07 -0400

    Faculty Resume

--- controllers/courses/helpers/insertCourse.js
+++ controllers/courses/helpers/insertCourse.js
@@ -1,23 +1,31 @@
-const course = require('../../../models/courses');
+const course = require("../../../models/courses");
 const jwt_decode = require("jwt-decode");
-const moment = require('moment');
+const moment = require("moment");
 exports.insertCourses = async (req, res) => {
-  const { path } = req.file
+  const { path } = req.file;
 
   try {
     const value = jwt_decode(req.headers.authorization);
     let date = moment().unix().toString();
     req.body.facultyResume = path;
     req.body.creation_date = date;
-    req.body.owner_id = value._id
+    req.body.owner_id = value._id;
     req.body.isDegree = false;
+    req.body.fac;
 
-    await course.collection.insertOne(req.body)
+    req.body.facultyInformation = [
+      {
+        facultyName: req.body.facultyName,
+        facultyResume: req.body.facultyResume,
+      },
+    ];
+
+    await course.collection.insertOne(req.body);
     res.json({
       success: true,
-      data: "Inserted"
-    })
+      data: "Inserted",
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
