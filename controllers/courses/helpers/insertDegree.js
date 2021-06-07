fb918c38bb18ac71824aaaf2e80351382bc985a3
babar-at-657557213670 <babar@websailors.co>
2021-06-07 08:47:41 -0400

    Insert Degree Solo

--- controllers/courses/helpers/insertDegree.js
+++ controllers/courses/helpers/insertDegree.js
@@ -1,53 +1,49 @@
-const courses = require('../../../models/courses');
-const moment = require('moment');
+const courses = require("../../../models/courses");
+const moment = require("moment");
 const jwt_decode = require("jwt-decode");
 
 exports.insertDegree = async (req, res) => {
-  let pathsArr = []
-  pathsArr = req.files.map((file) => file.path)
+  let pathsArr = [];
+  pathsArr = req.files.map((file) => file.path);
   try {
     req.body.creation_date = moment().unix().toString();
     req.body.isDegree = true;
-    req.body.facultyResume = pathsArr
 
-    let course=req.body.course.split(',');
-    req.body.course=course;
+    req.body.facultyResume = pathsArr;
 
-    let online=req.body.online.split(',');
-    req.body.online=online;
+    let course = JSON.parse(req.body.course);
 
-    let offline=req.body.offline.split(',');
-    req.body.offline=offline;
+    req.body.course = course;
 
-    let facultyInfo=req.body.facultyInfo.split(',');
-    req.body.facultyInfo=facultyInfo;
+    let facultyInfo = req.body.facultyInfo.split(",");
+    req.body.facultyInfo = facultyInfo;
 
-    let compulsory=req.body.compulsory.split(',');
-    req.body.compulsory=compulsory;
+    let compulsory = req.body.compulsory.split(",");
+    req.body.compulsory = compulsory;
 
+    let optional = req.body.optional.split(",");
+    req.body.optional = optional;
 
-    let optional=req.body.optional.split(",");
-    req.body.optional=optional;
+    let facultyInformation = [];
+    for (let i = 0; i < req.body.facultyInfo.length; i++) {
+      let obj = {};
+      (obj.facultyName = req.body.facultyInfo[i]),
+        (obj.facultyResume = req.body.facultyResume[i]);
 
-
-    let facultyInformation=[];
-    for(let i=0;i<req.body.facultyInfo.length;i++){
-      let obj={};
-        obj.facultyName=req.body.facultyInfo[i],
-        obj.facultyResume=req.body.facultyResume[i]
-
-        facultyInformation.push(obj);
+      facultyInformation.push(obj);
     }
-req.body.facultyInformation=facultyInformation;
-const value = jwt_decode(req.headers.authorization);
+    req.body.facultyInformation = facultyInformation;
+    const value = jwt_decode(req.headers.authorization);
 
-req.body.owner_id=value._id
-    await courses.collection.insertOne(req.body)
+    req.body.owner_id = value._id;
+
+    console.log("Last Object", req.body);
+    await courses.collection.insertOne(req.body);
     res.json({
       success: true,
-      data: "Inserted"
-    })
+      data: "Inserted",
+    });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
